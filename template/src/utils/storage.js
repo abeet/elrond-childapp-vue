7346b815e379dabb2bbd06cebe9bafb76be8d0c7
wangzhaohui <alex.wang@zving.com>
2018-10-17 22:01:26 +0800

    格式化

--- template/src/utils/storage.js
+++ template/src/utils/storage.js
@@ -1,30 +1,30 @@
 let SERVICEID // 子应用id
 
-function getKey(key) {
-    if (!SERVICEID) {
-        throw new Error('LocalStorage方法前需先调用LocalStorage.setServiceId设置子应用id')
-    }
-    return `${SERVICEID}.${key}`
+function getKey (key) {
+  if (!SERVICEID) {
+    throw new Error('LocalStorage方法前需先调用LocalStorage.setServiceId设置子应用id')
+  }
+  return `${SERVICEID}.${key}`
 }
 
-const LocalStorage = {};
+const LocalStorage = {}
 LocalStorage.setItem = function (key, value) {
-    window.localStorage.setItem(getKey(key), value)
+  window.localStorage.setItem(getKey(key), value)
 }
 
 LocalStorage.getItem = function (key) {
-    return window.localStorage.getItem(getKey(key));
+  return window.localStorage.getItem(getKey(key))
 }
 
 LocalStorage.removeItem = function (key) {
-    window.localStorage.removeItem(getKey(key));
+  window.localStorage.removeItem(getKey(key))
 }
 
 LocalStorage.setServiceId = function (serviceId) {
-    SERVICEID = serviceId.replace(/^\//, '')
+  SERVICEID = serviceId.replace(/^\//, '')
 }
 LocalStorage.getServiceId = function () {
-    return SERVICEID
+  return SERVICEID
 }
 
-export default LocalStorage;
+export default LocalStorage
